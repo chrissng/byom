e7a9498823872cec56dc7c2c971435e54d16652e
Chris Sng <chris@data.gov.sg>
2016-11-06 16:16:55 +0800

    stop osrm container also

--- stop.sh
+++ stop.sh
@@ -3,3 +3,4 @@
 docker-compose -p maps -f pelias-docker/docker-compose.yml down
 docker-compose -p maps -f tileserver-docker/docker-compose.yml down
 docker-compose -p maps -f valhalla-docker/docker-compose.yml down
+docker stop maps_osrm
