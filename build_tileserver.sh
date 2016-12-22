8b88d29d490f11211333e408e4a298e954bcfb78
Chris Sng <chrissng@users.noreply.github.com>
2016-12-22 10:37:19 +0800

    Update build_tileserver.sh

--- build_tileserver.sh
+++ build_tileserver.sh
@@ -6,7 +6,7 @@ set -eu
 set +e
 git clone \
   --branch=master \
-  https://github.com/datagovsg/tileserver-docker.git
+  https://github.com/chrissng/tileserver-docker.git
 git clone \
   --branch=master \
   https://github.com/osm2vectortiles/osm2vectortiles.git
